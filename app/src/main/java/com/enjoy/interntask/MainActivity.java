146ae20d92e6c3d7916c7f533cfe04da636f23f3
rimjhimsudo <rimjhimvaish@gmail.com>
2020-06-06 11:48:09 +0530

    fragments completd

--- app/src/main/java/com/enjoy/interntask/MainActivity.java
+++ app/src/main/java/com/enjoy/interntask/MainActivity.java
@@ -1,8 +1,10 @@
 package com.enjoy.interntask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
@@ -20,7 +22,34 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_dashboard_black_24dp));
         bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_account_box_black_24dp));
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(),"Clicked item"+item.getId(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment select_fragment=null;
+                switch (item.getId()){
+                    case ID_home:
+                        select_fragment=new HomeFragment();
+                        break;
+                    case ID_dashboard:
+                        select_fragment=new DashboardFragment();
+                        break;
+                    case ID_account:
+                        select_fragment=new AccountFragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,select_fragment).commit();
+            }
+        });
+
+
 
     }
 }
