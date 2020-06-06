36325b09795b2c293353ac860eb1d7db65aa8d34
rimjhimsudo <rimjhimvaish@gmail.com>
2020-06-06 15:45:33 +0530

    frstore completed

---
+++ app/src/main/java/com/enjoy/interntask/Daysadapter.java
@@ -0,0 +1,67 @@
+package com.enjoy.interntask;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Daysadapter  extends RecyclerView.Adapter<Daysadapter.Daysadaptervwholder> {
+    //public  String[] weedays;
+    ArrayList<String> daysofweek=new ArrayList<>();
+    public  Daysadapter(ArrayList daysofweek){
+
+        this.daysofweek=daysofweek;
+    }
+    @NonNull
+    @Override
+    public Daysadaptervwholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.home_cardvw,parent,false);
+
+
+        return new Daysadaptervwholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Daysadaptervwholder holder, int position) {
+        String day= daysofweek.get(position);
+        holder.day.setText(day);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysofweek.size();
+    }
+
+    public  class Daysadaptervwholder extends RecyclerView.ViewHolder{
+        ImageView imageVwdays;
+        TextView day;
+        public Daysadaptervwholder(final View itemview){
+            super(itemview);
+            imageVwdays=itemview.findViewById(R.id.imagevw);
+            day=itemview.findViewById(R.id.textvwday);
+            itemview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int id=getAdapterPosition();
+                    Intent intent=new Intent(itemView.getContext(),SecondActivity.class);
+                    intent.putExtra("Dayname",daysofweek.get(id).toString() );
+                    Log.d("DAY ",""+daysofweek.get(id));
+                    itemview.getContext().startActivity(intent);
+                }
+            });
+        }
+    }
+    public  interface  oncardviewListener{
+        void  oncardClick(int pos);
+    }
+}
--- app/src/main/java/com/enjoy/interntask/HomeFragment.java
+++ app/src/main/java/com/enjoy/interntask/HomeFragment.java
@@ -1,28 +1,63 @@
 package com.enjoy.interntask;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    Daysadapter daysadapter;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView=view.findViewById(R.id.recyclevw);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        //String[] daysofwee={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("Monday");
+        arrayList.add("Tuesday");
+        arrayList.add("Wednesday");
+        arrayList.add("Thursday");
+        arrayList.add("Friday");
+        arrayList.add("Saturday");
+        arrayList.add("Sunday");
+        daysadapter=new Daysadapter(arrayList);
+        recyclerView.setAdapter(daysadapter);
+
+        return view;
+
+
+
+
     }
+
+   /* @Override
+    public void oncardClick(int pos) {
+        //daysadapter.
+        Intent intent=new Intent(getActivity(),SecondActivity.class);
+        intent.putExtra("Dayname",)
+        startActivity(intent);
+    }*/
 }
