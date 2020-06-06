a277c1246aa3d2b106646074a9b7f9d0fe5fb7dc
rimjhimsudo <rimjhimvaish@gmail.com>
2020-06-06 11:31:03 +0530

    bottom nav bar added and fragmants

--- app/src/main/java/com/enjoy/interntask/MainActivity.java
+++ app/src/main/java/com/enjoy/interntask/MainActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
+public class MainActivity extends AppCompatActivity {
+    MeowBottomNavigation bottomNavigation;
+    private final static int ID_home=1;
+    private final static int ID_dashboard=2;
+    private final static int ID_account=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation= findViewById(R.id.bottomNavigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home_black_24dp));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_dashboard_black_24dp));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_account_box_black_24dp));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
 
     }
 }
